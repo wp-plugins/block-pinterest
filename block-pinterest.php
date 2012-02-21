73ad1706d4e9c7c36111d86cc7b0e5202d525a49
lgladdy <lgladdy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-21 19:35:51 +0000

    Adding 1.0.3 trunk
    git-svn-id: https://plugins.svn.wordpress.org/block-pinterest/trunk@508433 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- block-pinterest.php
+++ block-pinterest.php
@@ -4,7 +4,7 @@ Plugin Name: Block Pinterest
 Plugin URI: http://wordpress.org/extend/plugins/block-pinterest/
 Description: Adds the required meta tags to stop your blog and image content appearing on Pinterest
 Author: Liam Gladdy
-Version: 1.0
+Version: 1.0.3
 Author URI: http://www.twitter.com/lgladdy
 */
 
